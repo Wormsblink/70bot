98045784868fa82bceacbd19eb4d67869951b8f5
Wormsblink <wormsblink@gmail.com>
2021-05-10 17:47:59 +0800

    Add files via upload
    v0.4

--- amosyeebot.py
+++ amosyeebot.py
@@ -12,20 +12,21 @@ def bot_login():
                         password = config.password,
                         client_id = config.client_id,
                         client_secret = config.client_secret,
-                        user_agent = "Worms amos yee bot v0.3")
+                        user_agent = "Worms amos yee bot v0.4")
         print("Log in successful!")
         return r
 
 def run_bot(r, replied_comments_id):
         print("Retrieving last 25 comments")
-        for comment in r.subreddit('test').comments(limit = 10):
+        for comment in r.subreddit('test').comments(limit = 25):
                 if (("amos yee" in comment.body or "Amos yee" in comment.body or "Amos Yee" in comment.body) and comment.id not in replied_comments_id and not comment.author == r.user.me() and not "bot" in comment.author.name):
-                        print("found text")
+                        print("found matching text")
 
                         if (len(get_replied_comments())<2):
                                 comment.reply("First mention of Amos Yee!")
-                                print("First mention")
+                                #print("First mention")
                         else:
+                                #print("Not first mention")
                                 comment.reply(prepare_reply(r))
 
                         print("Replied to comment " + comment.id + " by " + comment.author.name)
@@ -38,12 +39,30 @@ def run_bot(r, replied_comments_id):
         time.sleep(10)
 
 def prepare_reply(r):
+
+        #print("trying to retrive last mention")
+        time_since_last_mention = get_time_since_last_mention(r)
+        #print("time since last mention: " + time_since_last_mention)
+
+        last_mention_author = get_last_mention(r).author.name
+        #print("last mention author: " + last_mention_author)
+
+        last_mention_date = datetime.utcfromtimestamp(get_last_mention(r).created_utc).strftime('%d %b %y')
+        #print("last mention date: " + last_mention_date)
+
+        last_mention_title = get_last_mention(r).submission.title
+        #print("last mention title: " + last_mention_title)
+
+        last_mention_link = get_last_mention(r).permalink
+        #print("last mention link: " + last_mention_link)
+
+
         s = str("🎉 **RESET THE COUNTER!!!** 🎉\n\n" +
-        "It has been *" + get_prefix() + "* **" + get_time_since_last_mention(r) + "** since we've had an intellectual discussion about Amos Yee! \n\n" +
-        "Last mentioned by " + get_last_mention(r).author.name + " on **" + datetime.utcfromtimestamp(get_last_mention(r).created_utc).strftime('%d %b %y') + "**: " +
-        "[" + get_last_mention(r).submission.title + "](" + get_last_mention(r).permalink + ")" + "\n\n" +
+        "It has been *" + get_prefix() + "* **" + time_since_last_mention + "** since we've had an intellectual discussion about Amos Yee! \n\n" +
+        "Last mentioned by " + last_mention_author + " on **" + last_mention_date + "**: " +
+        "[" + last_mention_title + "](" + last_mention_link + ")" + "\n\n" +
         "***\n\n" +
-        "[v0.3](" + "https://github.com/Wormsblink/amos_yee_bot" + ") by Worms_sg | PM AmosYee_bot if bot is down")
+        "[v0.4](" + "https://github.com/Wormsblink/amos_yee_bot" + ") by Worms_sg and hosted on PythonAnywhere | PM AmosYee_bot if bot is down")
 
         return s
 
@@ -65,25 +84,48 @@ def get_prefix():
 
 
 def get_last_mention(r):
+        #print ("getting comment ")
         return r.comment(get_replied_comments()[-2])
 
 def get_time_since_last_mention(r):
         last_comment_time = int(get_last_mention(r).created_utc)
         current_time = int(time.time())
         time_difference = current_time - last_comment_time
+        # print("time calculation complete " + str(time_difference))
         if (time_difference > 24*3600*2):
                 return (str(int(time_difference/24/3600)) + " days")
 
         elif(time_difference > 3600*2):
                 return(str(int(time_difference/3600)) + " hours")
-            
+
         elif(time_difference>60*2):
                 return(str(int(time_difference/60)) + " minutes")
         else:
         		return (str(time_difference) + " seconds")
 
+
 r = bot_login()
 while True:
-        run_bot(r, get_replied_comments())
+        try:
+                run_bot(r, get_replied_comments())
+        except:
+                print("Error running bot")
 
+                try:
+                        r = bot_login()
+                        run_bot(r, get_replied_comments())
+
+                except:
+
+                        list = get_replied_comments()
+                        print ("deleting comment " + list[-2])
+                        list =list [:-2]
+                        print (list)
+
+                        with open("replied_comments.txt", "w") as f:
+                                for item in list:
+                                        f.write(item + "\n")
+
+
+                        time.sleep(5)
 
