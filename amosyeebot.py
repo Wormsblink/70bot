b40615b2f21f0e79f065e4c480d83d4159272b36
Wormsblink <wormsblink@gmail.com>
2021-06-24 15:16:18 +0100

    v0.7

--- amosyeebot.py
+++ amosyeebot.py
@@ -18,17 +18,19 @@ def bot_login():
         return r
 
 def run_bot(r, replied_comments_id):
-        #print("Retrieving last 25 comments")
-        for comment in r.subreddit('test').comments(limit = 25):
-                if (("amos yee" in comment.body or "Amos yee" in comment.body or "Amos Yee" in comment.body) and comment.id not in replied_comments_id and not comment.author == r.user.me() and not "bot" in comment.author.name):
+        print("Retrieving last 50 comments")
+        for comment in r.subreddit('singapore').comments(limit = 50):
+                if (("amos yee" in comment.body or "Amos yee" in comment.body or "Amos Yee" in comment.body or "amosyee" in comment.body) and comment.id not in replied_comments_id and not comment.author == r.user.me() and not "bot" in comment.author.name):
                         print("found matching text")
 
-                        if (len(get_replied_comments())<2):
+                        if (len(replied_comments_id)<2):
                                 comment.reply("First mention of Amos Yee!")
-                                #print("First mention")
+                                print("First mention")
                         else:
                                 #print("Not first mention")
-                                comment.reply(prepare_reply(r))
+                                text_reply = prepare_reply(r)
+                                print("reply prepared")
+                                comment.reply(text_reply)
 
                         print("Replied to comment " + comment.id + " by " + comment.author.name)
                         replied_comments_id.append(comment.id)
@@ -40,6 +42,7 @@ def run_bot(r, replied_comments_id):
         time.sleep(10)
 
 def prepare_reply(r):
+        #print("DEBUG: function prepare_reply")
 
         #print("trying to retrive last mention")
         time_since_last_mention = get_time_since_last_mention(r)
@@ -57,17 +60,21 @@ def prepare_reply(r):
         last_mention_link = get_last_mention(r).permalink
         #print("last mention link: " + last_mention_link)
 
+        #print("attempting to string s")
 
         s = str("🎉 **RESET THE COUNTER!!!** 🎉\n\n" +
         "It has been *" + get_prefix() + "* **" + time_since_last_mention + "** since we've had an intellectual discussion about Amos Yee! \n\n" +
         "Last mentioned by " + last_mention_author + " on **" + last_mention_date + "**: " +
         "[" + last_mention_title + "](" + last_mention_link + ")" + "\n\n" +
         "***\n\n" +
-        "[v0.6](" + "https://github.com/Wormsblink/amos_yee_bot" + ") by Worms_sg and running on Raspberry Pi400 | PM AmosYee_bot if bot is down")
+        "[v0.7](" + "https://github.com/Wormsblink/amos_yee_bot" + ") by Worms_sg and running on Raspberry Pi400 | PM AmosYee_bot if bot is down")
+
+        #print("s concatenated")
 
         return s
 
 def get_replied_comments():
+        #print("DEBUG: function get_replied_comments")
         if not os.path.isfile("replied_comments.txt"):
                 replied_comments_id = []
         else:
@@ -78,17 +85,22 @@ def get_replied_comments():
         return replied_comments_id
 
 def get_prefix():
-		with open("prefix.txt") as f:
-				prefixes = f.read()
+		print("DEBUG: function get_prefix")
+		with open("prefix.txt") as f2:
+				prefixes = f2.read()
 				prefixes = prefixes.split("\n")
-		return random.choice(prefixes)
+		print("prefixes split")
+		chosen_prefix = random.choice(prefixes)
+		print("chosen prefix")
+		return chosen_prefix
 
 
 def get_last_mention(r):
-        #print ("getting comment ")
+        #print("DEBUG: function get_last_mention")
         return r.comment(get_replied_comments()[-2])
 
 def get_time_since_last_mention(r):
+        print("DEBUG: function get_time_since_last_mention")
         last_comment_time = int(get_last_mention(r).created_utc)
         current_time = int(time.time())
         time_difference = current_time - last_comment_time
@@ -105,6 +117,7 @@ def get_time_since_last_mention(r):
         		return (str(time_difference) + " seconds")
 
 def remove_deleted_comments(r):
+        #print("DEBUG: remove_deleted_comments")
 
         list = get_replied_comments()
 
@@ -123,16 +136,19 @@ def remove_deleted_comments(r):
 
 
 r = bot_login()
+print("clearing old comments")
+remove_deleted_comments(r)
+
 while True:
         try:
                 run_bot(r, get_replied_comments())
         except:
-                print("Error running bot")
+                print("unkown error 1 occured")
 
                 try:
                         r = bot_login()
                         run_bot(r, get_replied_comments())
 
                 except:
-                        print ("attempting to delete comments")
-                        remove_deleted_comments(r)
+                        print ("unkown error 2 occured")
+                        #remove_deleted_comments(r)
